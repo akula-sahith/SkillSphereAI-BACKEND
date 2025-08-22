dadbc6169c10d4f7ac3fd49472a5e18bef955f6d
Sahith Akula <akulasahith268@gmail.com>
2025-08-22 14:12:45 +0530

    Target added

--- src/main/java/com/MinTrio/SkillSphereAI/SkillSphereAiApplication.java
+++ src/main/java/com/MinTrio/SkillSphereAI/SkillSphereAiApplication.java
@@ -14,15 +14,15 @@ public class SkillSphereAiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SkillSphereAiApplication.class, args);
-		Student student =  context.getBean(Student.class);
-		student.setName("Sahith");
-		student.setEmail("akulasahith268@gmail.com");
-		List<String> skills = Arrays.asList("Java","Python");
-		student.setSkills(skills);
-		List<Integer> cids = Arrays.asList(1,2,3);
-		student.setCourse_ids(cids);
-		StudentService service = context.getBean(StudentService.class);
-		service.addStudent(student);
+//		Student student =  context.getBean(Student.class);
+//		student.setName("Sahith");
+//		student.setEmail("akulasahith268@gmail.com");
+//		List<String> skills = Arrays.asList("Java","Python");
+//		student.setSkills(skills);
+//		List<Integer> cids = Arrays.asList(1,2,3);
+//		student.setCourse_ids(cids);
+//		StudentService service = context.getBean(StudentService.class);
+//		service.addStudent(student);
 	}
 
 }
