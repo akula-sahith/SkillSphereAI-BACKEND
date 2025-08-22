07da77f79e938e7e0ab353f14af992df384cddb0
Sahith Akula <akulasahith268@gmail.com>
2025-08-22 13:47:37 +0530

    Target added

--- src/main/java/com/MinTrio/SkillSphereAI/controller/StudentController.java
+++ src/main/java/com/MinTrio/SkillSphereAI/controller/StudentController.java
@@ -2,10 +2,7 @@ package com.MinTrio.SkillSphereAI.controller;
 
 import com.MinTrio.SkillSphereAI.model.Student;
 import com.MinTrio.SkillSphereAI.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -17,7 +14,7 @@ public class StudentController {
         return "Hello World";
     }
 
-    @GetMapping("/addStudent")
+    @PostMapping("/addStudent")
     public void addStudent(@PathVariable Student student){
         studentService.addStudent(student);
     }
